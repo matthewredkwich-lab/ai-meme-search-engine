80aecade041f076bed5558439ccb1707ab5f3feb
matthewredkwich-lab <matthewredkwich@gmail.com>
2025-08-26 12:52:34 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,22 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'bodo0tgbs4falkp7.public.blob.vercel-storage.com',
-        port: '',
+        protocol: "https",
+        hostname: "bodo0tgbs4falkp7.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'kqznk1deju1f3qri.public.blob.vercel-storage.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "kqznk1deju1f3qri.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "7jkfhhmnryelskzz.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
       },
     ],
     minimumCacheTTL: 60,
